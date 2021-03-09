b9c48a665ec085bac823ca8e3da5ff46d76ccfbf
Nahall Mansouri <Nahall.Mansouri-1@ou.edu>
2021-03-09 17:54:53 -0600

    Initial Push

---
+++ SafeSpaceCapstone/View/TeacherView.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SafeSpaceCapstone.View
+{
+    /// <summary>
+    /// Interaction logic for TeacherView.xaml
+    /// </summary>
+    public partial class TeacherView : Window
+    {
+        public TeacherView()
+        {
+            InitializeComponent();
+        }
+        #region Variables
+        string studentNamesFilePath;
+        string studentDataFilePath;
+        #endregion
+
+        #region Event Handlers
+        private void StudentNamesMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Title = "Select Student Names CSV...",
+                Filter = "CSV files(*.csv) | *.csv",
+                Multiselect = false
+            };
+            Mouse.OverrideCursor = Cursors.Wait;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                studentNamesFilePath = openFileDialog.FileName;
+            }
+        }
+
+        private void StudentDataMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Title = "Select Student Names CSV...",
+                Filter = "CSV files(*.csv) | *.csv",
+                Multiselect = false
+            };
+            Mouse.OverrideCursor = Cursors.Wait;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                studentDataFilePath = openFileDialog.FileName;
+            }
+        }
+        #endregion
+    }
+}
